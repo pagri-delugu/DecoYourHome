aa968d1b22c750d2d949f8515c2cd3f024f15dc2
Tan Phuc <starlinganime9907@gmail.com>
2026-08-04 21:13:31 +0700

    #4 - Created base.html and added style.css file
    Still need better designs in the future but this will do

--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+# from .forms import contactForm, reviewForm
+from .models import Product
 
-# Create your views here.
+# BASE TEMPLATE, DO NOT TOUCH!!!
+def base(request):
+    return render(request, 'pages/base.html')
+
+# DYNAMIC TEMPLATES BELOW
